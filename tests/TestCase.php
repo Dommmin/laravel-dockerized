303abde57e28d86de02091b4d6c63818dce7f49d
Dominik <d.jasinski@ideosoftware.com>
2024-12-16 18:57:54 +0100

    feat: create Docker config

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,22 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Http\Middleware\ValidateCsrfToken;
+use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        VerifyCsrfToken::except([
+            '*'
+        ]);
+
+        ValidateCsrfToken::except([
+            '*'
+        ]);
+    }
 }
